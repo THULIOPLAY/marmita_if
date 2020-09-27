608db7a8a4ad483f056db63f10ccea942f91eba8
THULIOPLAY <35309045+THULIOPLAY@users.noreply.github.com>
2020-09-27 02:52:54 -0300

    upd

--- src/Config.php
+++ src/Config.php
@@ -7,7 +7,7 @@ class Config {
     const DB_DRIVER = 'mysql';
     const DB_HOST = 'localhost';
     const DB_DATABASE = 'marmita_if';
-    CONST DB_USER = 'root';
+    const DB_USER = 'root';
     const DB_PASS = '';
 
     const ERROR_CONTROLLER = 'ErrorController';
